5d20ee7a4ad694232f7b83c45bfa69eed5bb7594
Coltan <jihoonog@gmail.com>
2018-06-24 09:42:08 -0600

    Made better visualizations for the tape.

--- main.cc
+++ main.cc
@@ -14,15 +14,16 @@ char* dataptr=strip;
 int bracketCounter;
 
 void showStrip() {
+    std::cout<<"Strip: ";
     for(int i = 0; i < 20; ++i){
         std::cout<<(int)strip[i]<<" ";
     }
-    std::cout<<"\n";
+    std::cout<<"\n       ";
     for(unsigned int i = 0; i < (dataptr - strip); ++i){
         std::cout<<"  ";
     }
     std::cout<<"^\n";
-    std::cout<<command<<"\n";
+    std::cout<<"Commands: "<<command<<"\n";
 }
 
 unsigned int gotoCorrespondingClosingBracket(){
@@ -58,10 +59,13 @@ void interpretCommand(char c){
             --*dataptr;
             break;
         case '.':
+            std::cout<<"\n\n";
+            std::cout<<"Byte at cell "<<(dataptr - strip)<<" :";
             std::putchar(*dataptr);
+            std::cout<<"\n\n";
             break;
         case ',':
-            std::cout<<"Input a byte for data cell "<<*dataptr<<" :";
+            std::cout<<"Input a byte for data cell "<<(dataptr - strip)<<" :";
             char temp; std::cin>>temp;
             *dataptr = temp;
             std::cout<<"\n";
@@ -92,11 +96,13 @@ void interpretCommand(char c){
 int main(){
     std::cout<<"BrainFuck interpreter\n\n";
     showStrip();
+    std::cout<<"\n";
     while(std::cin>>command){
         for(instructPtr = 0; instructPtr < command.size(); ++instructPtr){
             interpretCommand(command[instructPtr]);
         }
         showStrip();
+        std::cout<<"\n";
     }
     return 0;
 }
